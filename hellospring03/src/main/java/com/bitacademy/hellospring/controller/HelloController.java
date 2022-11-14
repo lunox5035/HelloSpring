1d91c865b5f18f8012fa87466e6ea10758c6bd7b
GawnGyu Yeo <bbc02131@naver.com>
2022-11-14 16:16:23 +0900

    spring 연습(오타주의)2

--- hellospring03/src/main/java/com/bitacademy/hellospring/controller/HelloController.java
+++ hellospring03/src/main/java/com/bitacademy/hellospring/controller/HelloController.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
 
 	@RequestMapping("/spring")
-	public String spring(String name, Model model) {
-		model.addAttribute("name",name);
-		return "/WEB-INF/views/spring.jsp";
+	public String spring() {				//handler
+		return"/WEB-INF/views/spring.jsp";
 	}
+	
+//	@RequestMapping("/spring")
+//	public String spring(String name, Model model) {
+//		model.addAttribute("name",name);
+//		return "/WEB-INF/views/spring.jsp";
+//	}
 }
