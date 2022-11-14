60918605b3cd5cd3ebf9b4b46129216453dfabe7
GawnGyu Yeo <bbc02131@naver.com>
2022-11-14 16:50:28 +0900

    haha

--- hellospring03/src/main/java/com/bitacademy/hellospring/controller/HelloController.java
+++ hellospring03/src/main/java/com/bitacademy/hellospring/controller/HelloController.java
@@ -7,14 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-	@RequestMapping("/spring")
-	public String spring() {				//handler
-		return"/WEB-INF/views/spring.jsp";
-	}
-	
 //	@RequestMapping("/spring")
-//	public String spring(String name, Model model) {
-//		model.addAttribute("name",name);
-//		return "/WEB-INF/views/spring.jsp";
+//	public String spring() {				//handler
+//		return"/WEB-INF/views/spring.jsp";
 //	}
+	
+	@RequestMapping("/spring")
+	public String spring(String name, Model model) {
+		
+		System.out.println("hello");
+		model.addAttribute("name",name);
+		return "/WEB-INF/views/spring.jsp";
+	}
 }
